8eae96cfd2313e7946ef8400a8f80a762627aca5
Michael Mattson <michael.p.mattson@gmail.com>
2021-07-13 16:29:41 -0600

    Add get all and grouped dishes methods

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -10,4 +10,13 @@ class Potluck
   def add_dish(name)
     @dishes.push(name)
   end
+
+  # test this
+  def grouped_dishes
+    grouped_dishes = @dishes.group_by { |dish| dish.category }
+  end
+
+  def get_all_from_category(category)
+    grouped_dishes[category]
+  end
 end
