c68fe820aeff84817a66922d62eb12e31f6c58cb
Michael Mattson <michael.p.mattson@gmail.com>
2021-07-13 13:41:16 -0600

    Add dishes empty array test

--- spec/potluck_spec.rb
+++ spec/potluck_spec.rb
@@ -14,5 +14,11 @@ RSpec.describe Potluck do
 
       expect(potluck.date).to eq("7-13-18")
     end
+
+    it 'has an empty dishes array' do
+      potluck = Potluck.new("7-13-18")
+
+      expect(potluck.dishes).to eq([])
+    end
   end
 end
