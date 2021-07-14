8ab54ca3f9bcc4a265201a34b5e597a720ae4cb4
Michael Mattson <michael.p.mattson@gmail.com>
2021-07-13 18:09:08 -0600

    Last 2 methods

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -16,7 +16,35 @@ class Potluck
     grouped_dishes = @dishes.group_by { |dish| dish.category }
   end
 
+  def categories
+    grouped_dishes.keys
+  end
+
   def get_all_from_category(category)
     grouped_dishes == {} ? [] : grouped_dishes[category]
+  end
+
+  def menu
+    hash = {}
+    categories.each do |category|
+      hash[plural(category)] = grouped_dishes_array(category)
+    end
+    hash
+  end
+
+  def grouped_dishes_array(category)
+    grouped_dishes[category].map { |dish| dish.name }.sort
+  end
+
+  def plural(symbol)
+    symbol = (symbol.to_s + "s").to_sym
+  end
+
+  def ratio(category)
+    (grouped_dishes_array(category).length.to_f / total_dishes) * 100
+  end
+
+  def total_dishes
+    @dishes.length
   end
 end
